62eb879140aece27c5bb1f23914ce86e38694146
Joel Scott <JKScott@tql.com>
2020-09-02 14:39:04 -0400

    added storefront skeleton components

---
+++ StoreFront/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA0N_iQNSOU808HervHniMJ46zTBMOotVw",
+    authDomain: "storefrontjs.firebaseapp.com",
+    databaseURL: "https://storefrontjs.firebaseio.com",
+    projectId: "storefrontjs",
+    storageBucket: "storefrontjs.appspot.com",
+    messagingSenderId: "739392150345",
+    appId: "1:739392150345:web:ebacf90f097f75377f1898"
+  }
+};
---
+++ StoreFront/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA0N_iQNSOU808HervHniMJ46zTBMOotVw",
+    authDomain: "storefrontjs.firebaseapp.com",
+    databaseURL: "https://storefrontjs.firebaseio.com",
+    projectId: "storefrontjs",
+    storageBucket: "storefrontjs.appspot.com",
+    messagingSenderId: "739392150345",
+    appId: "1:739392150345:web:ebacf90f097f75377f1898"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
